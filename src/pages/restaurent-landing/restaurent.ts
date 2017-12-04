2ed8b205e8d9bfbe8ac1ff8292f71855f91a5433
senthilpsk <senthilpsk@users.noreply.github.com>
2017-12-04 18:45:23 +0530

    changes by jeswin

--- src/pages/restaurent-landing/restaurent.ts
+++ src/pages/restaurent-landing/restaurent.ts
@@ -198,7 +198,12 @@ export class RestaurentPage extends BasePage {
                                 this.places = data;
                                 this.showContentView();
                                 this.onRefreshComplete();
-
+                                event.target.classList.add('activeli');
+                                this.near.nativeElement.classList.remove("activeli");
+                                this.dine.nativeElement.classList.remove("activeli");
+                                this.take.nativeElement.classList.remove("activeli");
+                                this.deliver.nativeElement.classList.remove("activeli");
+                                this.party.nativeElement.classList.remove("activeli");
                             });
                         }).catch((error) => {
                             this.showContentView();
